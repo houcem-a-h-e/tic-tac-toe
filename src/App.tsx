45676f8363ff2c7cadf3ff8fcc64daeb2225ce37
houcem-a-he <houcemeddine.aouissaoui@esprit.tn>
2024-10-26 11:13:13 +0200

    Initial project setup with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './index.css';
+import Square from './components/Square';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Square value="X" onClick={()=>console.log("clicked")} />
     </div>
   );
 }
---
+++ src/components/Square.tsx
@@ -0,0 +1,17 @@
+interface SquareProps {
+    value: 'X' | 'O' | 'null';
+    onClick: () => void;
+}
+
+const Square: React.FC<SquareProps> = ({ value, onClick }) => {
+    return (
+        <div
+            className="w-20 h-20 bg-red-500 border border-gray-300 flex items justify-center text-4xl cursor-pointer hover:bg-gray-100 transition duration-200"
+            onClick={onClick}
+        >
+            {value}
+        </div>
+    );
+}
+
+export default Square;
